f481c915439e7e43fa2b1afd3cfdceee96bbdbcb
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-07-05 12:19:09 +0530

    added operators file

---
+++ operators.py
@@ -0,0 +1,115 @@
+# OPERATORS
+
+    # 1) Arithmetic Operators:-
+# x = 5
+# y = 2
+
+        # Addition +
+# print(f"{x} and {y} Addition is : {x + y}")
+
+        # Subtracction -
+# print(f"{x} and {y} Subtraction is : {x  - y}")
+
+        # Multiplication *
+# print(f"{x} and {y} Multiplication is : {x * y}")
+
+        # Normal Division /
+# print(f"{x} and {y} Normal Division is : {x / y}")
+
+        # Floor Division  //
+# print(f"{x} and {y} Floor Division is : {x // y}")
+
+        # Power **
+# print(f"{x} Power is {y} : {x ** y}")
+
+        # Mod %
+# print(f"{x} Mod {y} is : {x + y}")
+
+    # 2) Assignment Operator:-
+        # To assign a value to a variabble we use =
+# m = 10
+# n = "fathima"
+
+x = 5
+
+        # add and assign operator +=
+# x = x + 3
+# print(x)
+# x+=3
+# print(x)
+
+        # subtract and assign operator -=
+# x-=2
+# print(x)
+        # multiply and assign operator *=
+# x *= 4
+# print(x)
+        # normal division and assign operator /=
+# x /= 5
+# print(x)
+
+        # Floor division and  assign operator //=
+# x //= 3
+# print(x)
+
+        # Power and assign **=
+# x **=2
+# print(x)
+
+   
+        # mod and assign %=
+# x %=7
+# print(x)
+
+    # 3) Comparison Operator:-
+x = 5
+y = 2
+
+# print(x == y)
+# print(x > y)
+# print(x < 2)
+# print(x != y)
+# print(x <= y)  #  both the conditions are false --> false
+# print(x >= y)  # at least one condition is true --> true
+
+    # 4) Membership Operator:-
+        # 1) in
+        # 2) not in
+# str1 = "Apple"
+
+# print('l' in str1)
+# print('A' in str1)
+# print('E' in str1)
+
+# l1 = ['A', 23, 45.5, 9-7j, 100, 'Apple']
+# print(100 in l1)
+
+# tup1 = (1, 2, 3, 4, 5, 6)
+# print(23 not in tup1)
+
+# set1 = {4, 5, 6, 7, 3, 2, 4, 5}
+# print(4 in set1)
+
+    # 5) Identity Operator:-
+# m =  100
+# n = 1000
+# print(m is n)
+# print(id(m))
+# print(id(n))
+
+# x = 'Apple'
+
+# y = 'apple'.title()
+
+
+# print(x)
+# print(id(x))
+
+# print(y)
+# print(id((y)))
+
+# print(x is y)
+
+
+    # 6) ternary Operator:-
+    # 7) Bitwise Operator:-
