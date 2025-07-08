9e3059cb342b3d6d0539d6ac3071db6d15b4650f
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-07-08 10:03:11 +0530

    added conditional statements

---
+++ conditionalstatements.py
@@ -0,0 +1,57 @@
+# print("Welcomme to 'MARRIAGE ELIGIBILITY APP'.")
+
+# age = int(input("Enter your age: "))
+
+# if age == 0 or age < 0:
+#     print("Please enter a valid age..!")
+    
+# if age > 0 and age < 18:
+#     print("You are still kid . not eligible to get married")
+    
+# if age == 18:
+#     print("Congrats..! you turned 18 and got eleigible to get married")
+    
+# if age > 18 and age <35:
+#     print("Please get married you are allowed")
+
+# if age >= 35 and age < 50:
+#     print("You got late to get marrieed but its okay. get married soon.")
+    
+# if age > 50:
+#     print("Man..! Try in next life")
+
+#############################################
+
+# Only there are two conditions:
+    # if 
+    # else
+    
+# number = int(input("enter a number: "))
+
+# if number  % 2 == 0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# isMarried = input("Are you married[Y/n]: ")
+
+# if isMarried.upper() == "Y":
+#     print("Yes ..! You are married")
+# else:
+#     print("No ..! You are not married")
+    
+
+# If more than two conditions:
+    # if
+    # elif  --> else+if
+    # elif
+    # else
+    
+# Ask user to enter a salary:
+
+# condition1 : if salary is less than 10000 --> Economically backward
+# condition2 : if salary is greater than 10000 and lesser than 20000 --> Economically lower middle class
+# condition3 : if salary is greater than 20000 and lesser than 50000 --> Economically middle class
+# condition4 : if salary is greater than 50000 and lesser than 100000 ---> Economically upper middle class
+# condition5 : if salary is greater than 100000 ---> Economically upper class
+
