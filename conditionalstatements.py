732de9bbf04470ded38f550705ceb77443e61f97
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-07-09 10:18:45 +0530

    Added nested conditions

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -55,3 +55,129 @@
 # condition4 : if salary is greater than 50000 and lesser than 100000 ---> Economically upper middle class
 # condition5 : if salary is greater than 100000 ---> Economically upper class
 
+####################################################
+
+# Nested Conditions
+
+# Ask user to enter a number in between 0 to 100
+    # condition1 :- if number is in between 0-25 --> quarter no.1
+        # condition1A:- if number is even print "Even"
+        # condition1B:- if number is odd print "Odd"
+        
+     # condition2 :- if number is in between 26-50 --> quarter no.2
+        # condition2A:- if number is even print "Even"
+        # condition2B:- if number is odd print "Odd"
+        
+    # condition3 :- if number is in between 51-75 --> quarter no.3
+        # condition3A:- if number is even print "Even"
+        # condition3B:- if number is odd print "Odd"
+        
+    # condition4 :- if number is in between 76-100 --> quarter no.4
+        # condition4A:- if number is even print "Even"
+        # condition4B:- if number is odd print "Odd"
+        
+
+# num = int(input("Enter a number in between 0 to 100: "))
+
+# if num >= 0 and num < 26:
+#     print("quarter no. 1")
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# elif num >= 26 and num < 51:
+#     print("quarter no. 2")
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+        
+# elif num >= 51 and num < 76:
+#     print("quarter no. 3")
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+        
+# elif num >= 76 and num < 101:
+#     print("quarter no. 4")
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+        
+# else:
+#     print("Entered incorrect number.\nPLease eneter number from 0 to 100 range.")
+    
+    
+# ask user to enter a number, first check wether number is
+# even or odd, if  even print "Even" if odd print "odd".
+# and then check number is divisible by 3 & 5 both
+# if divisible print "Divisible" if not print "Not Divisible"
+
+# number = int(input("Eenter a number: "))
+
+# if number % 2 == 0:
+#     print("Even")
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("Divisible")
+    
+#     else:
+#         print("Not Divisible")
+# else:
+#     print("Odd")
+    
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("Divisible")
+    
+#     else:
+#         print("Not Divisible")
+    
+
+# str1 = 'Hello How are you ?'
+
+# check wether string in title case, upper case, lower case, 
+# capitalize case or mixed case.
+# is there any special charecter in string or not.
+# if it is there print "yes there is a special charecter"
+# else print "No there is no special charecter"
+
+
+# if str1.islower():
+#     print("Lower")
+#     if " " in str1 or "@" in str1 or "!" in str1 or "." in str1 or "?" in str1:
+#         print("Yes there is a special charecter")
+#     else:
+#         print("No Special Charecter")
+        
+# elif str1.isupper():
+#     print("Upper")
+    
+#     if " " in str1 or "@" in str1 or "!" in str1 or "." in str1 or "?" in str1:
+#         print("Yes there is a special charecter")
+#     else:
+#         print("No Special Charecter")
+    
+# elif str1.istitle():
+#     print("title")
+    
+#     if " " in str1 or "@" in str1 or "!" in str1 or "." in str1 or "?" in str1:
+#         print("Yes there is a special charecter")
+#     else:
+#         print("No Special Charecter")
+    
+# elif str1[0].isupper() and str1[1::].islower():
+#     print("Capitalize")
+    
+#     if " " in str1 or "@" in str1 or "!" in str1 or "." in str1 or "?" in str1:
+#         print("Yes there is a special charecter")
+#     else:
+#         print("No Special Charecter")
+    
+# else:
+#     print("Mixed")
+#     if " " in str1 or "@" in str1 or "!" in str1 or "." in str1 or "?" in str1:
+#         print("Yes there is a special charecter")
+#     else:
+#         print("No Special Charecter")
