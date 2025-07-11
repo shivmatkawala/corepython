d0c9571b9fdf07af71d853206afe590de0014abb
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-07-11 10:13:04 +0530

    added control statements file

---
+++ controlstatements.py
@@ -0,0 +1,98 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
+
+#######################
+
+# for i in range(1, 1100):
+#     print(i)
+    
+    
+# num = 1
+# while num != 110:
+#     print(num)
+#     num +=1
+    
+    
+####################################################
+
+# while loop 
+
+# print 12 to 25 all numbers.
+
+# number = 12
+
+# while number != 25:
+#     print(number)
+#     number += 1
+
+
+# print all the even numbers from 12 to 25
+
+# number = 12
+
+# while number != 25:
+#     if number % 2 == 0:
+#         print(number)
+#     else:
+#         pass
+#     number +=1
+    
+
+# print all the numbers from 0 to -20 
+
+# number = -20
+
+# while number != 0:
+#     print(number)
+#     number +=1
+    
+    
+# number = 0
+
+# while number != -20:
+#     print(number)
+#     number -=1
+    
+    
+# print all odd numbers from -32 t0 -44
+
+# number = -44
+
+# while number != -32:
+#     if number % 2 == 1:
+#         print(number)
+#     number +=1
+    
+
+# number = -32
+
+# while number != -44:
+#     if number % 2 != 0:
+#         print(number)
+    
+#     else:
+#         pass
+#     number -=1
+    
+    
+str1 = "Hello How are you ?"
+
+# print only vowels from str1
+
+
+count = 0
+
+while count < len(str1):
+    if str1[count].lower() in "aeiou":
+        print(str1[count]) 
+    count +=1
+    
+    
